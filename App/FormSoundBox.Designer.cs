935e7ee0d8c5edf8967442d6c3cc62f9acdc85a9
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2020-11-18 20:36:09 +0100

    Release commit

---
+++ App/FormSoundBox.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SoundBox
+{
+    partial class FormSoundBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSoundBox));
+            this.flpSounds = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flpSounds
+            // 
+            this.flpSounds.AutoScroll = true;
+            this.flpSounds.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flpSounds.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpSounds.Location = new System.Drawing.Point(0, 0);
+            this.flpSounds.Margin = new System.Windows.Forms.Padding(0);
+            this.flpSounds.Name = "flpSounds";
+            this.flpSounds.Padding = new System.Windows.Forms.Padding(50, 25, 50, 25);
+            this.flpSounds.Size = new System.Drawing.Size(800, 450);
+            this.flpSounds.TabIndex = 0;
+            // 
+            // FormSoundBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flpSounds);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormSoundBox";
+            this.Text = "SoundBox v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpSounds;
+    }
+}
+
---
+++ App/FormSoundBox.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using WMPLib;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SoundBox
+{
+    public partial class FormSoundBox : Form
+    {
+        private static List<WindowsMediaPlayer> theSounds;
+        private List<Button> theButtons;
+        private static List<string> allowedExtensions;
+        private static List<string> soundFiles;
+        private static string mediaFolder;
+
+        static FormSoundBox()
+        {
+            mediaFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\SoundBox\Media\";
+            allowedExtensions = new List<string>();
+            allowedExtensions.AddRange(new[] { ".aac", ".aif", ".aup", ".cda", ".flac", ".m3u", ".m4a", ".mid", ".midi", ".mp3", ".mpa", ".oga", ".ogg", ".ra", ".ram", ".wav", ".wma" });
+            theSounds = new List<WindowsMediaPlayer>();
+            if (!Directory.Exists(mediaFolder))
+            {
+                Directory.CreateDirectory(mediaFolder);
+            }
+            soundFiles = Directory.EnumerateFiles(mediaFolder).Where(aFile => allowedExtensions.Any(aFile.ToLower().EndsWith)).ToList();
+            if(soundFiles.Count == 0)
+            {
+                MessageBox.Show("Aucun son trouvé, vous pouvez en ajouter dans le dossier " + mediaFolder);
+                Environment.Exit(-1);
+            }
+            else
+            {
+                string aSoundFile;
+                for (int ind = 0; ind < soundFiles.Count; ind += 1)
+                {
+                    aSoundFile = soundFiles[ind];
+                    CreateSoundPlayer(aSoundFile);
+                }
+            }
+        }
+        public FormSoundBox()
+        {
+            InitializeComponent();
+            this.theButtons = new List<Button>();
+            for (int ind = 0; ind < soundFiles.Count; ind += 1)
+            {
+                CreateButton(ind);
+                this.flpSounds.Controls.Add(theButtons[ind]);
+            }
+        }
+
+        private static void CreateSoundPlayer(string soundPath)
+        {
+            WindowsMediaPlayer aSoundPlayer = new WindowsMediaPlayer();
+            aSoundPlayer.URL = soundPath;
+            aSoundPlayer.controls.stop();
+            theSounds.Add(aSoundPlayer);
+        }
+        private void CreateButton(int ind)
+        {
+            Button aButton = new Button();
+            aButton.Name = "btnSound" + ind;
+            aButton.Location = new Point(200 * ind + 100, 50);
+            aButton.Size = new Size(200, 50);
+            aButton.TabIndex = ind;
+            aButton.Text = Path.GetFileNameWithoutExtension(theSounds[ind].URL);
+            aButton.UseVisualStyleBackColor = true;
+            aButton.Click += new EventHandler(btnSound_Click);
+            this.theButtons.Add(aButton);
+        }
+        private void btnSound_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool isPlaying = false;
+                int ind = -1;
+                while (!isPlaying && ind < (theSounds.Count - 1))
+                {
+                    ind += 1;
+                    isPlaying = theSounds[ind].status.StartsWith("Lecture");
+                }
+                if (!isPlaying)
+                {
+                    theSounds[this.flpSounds.Controls.IndexOf((Control)sender)].controls.play();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
